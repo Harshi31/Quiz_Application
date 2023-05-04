d4e25cb53b431fe72b4f336035dc52ce636561f6
Harshita <rathoreharshita11@gmail.com>
2023-05-04 11:19:36 +0530

    copying

--- subjects_server/cloud.js
+++ subjects_server/cloud.js
@@ -7,167 +7,167 @@ const cloudQuestions = [
   {
     question: "What is the main benefit of cloud computing?",
     options: [
-      "Reduced cost of infrastructure",
-      "Faster processing speeds",
-      "Higher security measures",
+      "(A) Reduced cost of infrastructure",
+      "(B) Faster processing speeds",
+      "(C) Higher security measures",
       "More physical control over hardware",
     ],
-    answer: "Reduced cost of infrastructure",
+    answer: "(A) Reduced cost of infrastructure",
   },
   {
     question:
       "Which type of cloud deployment involves a single organization using a private cloud infrastructure?",
     options: [
-      "Public cloud",
-      "Community cloud",
-      "Hybrid cloud",
-      "Private cloud",
+      "(A) Public cloud",
+      "(B) Community cloud",
+      "(C) Hybrid cloud",
+      "(D) Private cloud",
     ],
-    answer: "Private cloud",
+    answer: "(D) Private cloud",
   },
   {
     question:
       "Which of the following is a characteristic of Infrastructure as a Service (IaaS)?",
     options: [
-      "The provider manages the operating system and applications",
-      "Customers have complete control over the underlying infrastructure",
-      "The provider manages the physical hardware",
-      "Customers only pay for the services they use",
+      "(A) The provider manages the operating system and applications",
+      "(B) Customers have complete control over the underlying infrastructure",
+      "(C) The provider manages the physical hardware",
+      "(D) Customers only pay for the services they use",
     ],
     answer:
-      "Customers have complete control over the underlying infrastructure",
+      "(B) Customers have complete control over the underlying infrastructure",
   },
   {
     question:
       "Which cloud computing model provides the most flexibility to customers?",
     options: [
-      "Infrastructure as a Service (IaaS)",
-      "Platform as a Service (PaaS)",
-      "Software as a Service (SaaS)",
-      "All three models provide the same level of flexibility",
+      "(A) Infrastructure as a Service (IaaS)",
+      "(B) Platform as a Service (PaaS)",
+      "(C) Software as a Service (SaaS)",
+      "(D) All three models provide the same level of flexibility",
     ],
-    answer: "Infrastructure as a Service (IaaS)",
+    answer: "(A) Infrastructure as a Service (IaaS)",
   },
   {
     question:
       "Which cloud service model is most appropriate for an organization that wants to develop and deploy its own applications?",
     options: [
-      "Infrastructure as a Service (IaaS)",
-      "Platform as a Service (PaaS)",
-      "Software as a Service (SaaS)",
-      "Hybrid cloud",
+      "(A) Infrastructure as a Service (IaaS)",
+      "(B) Platform as a Service (PaaS)",
+      "(C) Software as a Service (SaaS)",
+      "(D) Hybrid cloud",
     ],
-    answer: "Platform as a Service (PaaS)",
+    answer: "(B) Platform as a Service (PaaS)",
   },
   {
     question: "What is the main benefit of cloud computing?",
     options: [
-      "Reduced cost of infrastructure",
-      "Faster processing speeds",
-      "Higher security measures",
-      "More physical control over hardware",
+      "(A) Reduced cost of infrastructure",
+      "(B) Faster processing speeds",
+      "(C) Higher security measures",
+      "(D) More physical control over hardware",
     ],
-    answer: "Reduced cost of infrastructure",
+    answer: "(A) Reduced cost of infrastructure",
   },
   {
     question:
       "Which type of cloud deployment involves a single organization using a private cloud infrastructure?",
     options: [
-      "Public cloud",
-      "Community cloud",
-      "Hybrid cloud",
-      "Private cloud",
+      "(A) Public cloud",
+      "(B) Community cloud",
+      "(C) Hybrid cloud",
+      "(D) Private cloud",
     ],
-    answer: "Private cloud",
+    answer: "(D) Private cloud",
   },
   {
     question:
       "Which of the following is a characteristic of Infrastructure as a Service (IaaS)?",
     options: [
-      "The provider manages the operating system and applications",
-      "Customers have complete control over the underlying infrastructure",
-      "The provider manages the physical hardware",
-      "Customers only pay for the services they use",
+      "(A) The provider manages the operating system and applications",
+      "(B) Customers have complete control over the underlying infrastructure",
+      "(C) The provider manages the physical hardware",
+      "(D) Customers only pay for the services they use",
     ],
     answer:
-      "Customers have complete control over the underlying infrastructure",
+      "(B) Customers have complete control over the underlying infrastructure",
   },
   {
     question:
       "Which cloud computing model provides the most flexibility to customers?",
     options: [
-      "Infrastructure as a Service (IaaS)",
-      "Platform as a Service (PaaS)",
-      "Software as a Service (SaaS)",
-      "All three models provide the same level of flexibility",
+      "(A) Infrastructure as a Service (IaaS)",
+      "(B) Platform as a Service (PaaS)",
+      "(C) Software as a Service (SaaS)",
+      "(D) All three models provide the same level of flexibility",
     ],
-    answer: "Infrastructure as a Service (IaaS)",
+    answer: "(A) Infrastructure as a Service (IaaS)",
   },
   {
     question:
       "Which cloud service model is most appropriate for an organization that wants to develop and deploy its own applications?",
     options: [
-      "Infrastructure as a Service (IaaS)",
-      "Platform as a Service (PaaS)",
-      "Software as a Service (SaaS)",
-      "Hybrid cloud",
+      "(A) Infrastructure as a Service (IaaS)",
+      "(B) Platform as a Service (PaaS)",
+      "(C) Software as a Service (SaaS)",
+      "(D) Hybrid cloud",
     ],
-    answer: "Platform as a Service (PaaS)",
+    answer: "(B) Platform as a Service (PaaS)",
   },
   {
     question: "What is the main benefit of cloud computing?",
     options: [
-      "Reduced cost of infrastructure",
-      "Faster processing speeds",
-      "Higher security measures",
-      "More physical control over hardware",
+      "(A) Reduced cost of infrastructure",
+      "(B) Faster processing speeds",
+      "(C) Higher security measures",
+      "(D) More physical control over hardware",
     ],
-    answer: "Reduced cost of infrastructure",
+    answer: "(A) Reduced cost of infrastructure",
   },
   {
     question:
       "Which type of cloud deployment involves a single organization using a private cloud infrastructure?",
     options: [
-      "Public cloud",
-      "Community cloud",
-      "Hybrid cloud",
-      "Private cloud",
+      "(A) Public cloud",
+      "(B) Community cloud",
+      "(C) Hybrid cloud",
+      "(D) Private cloud",
     ],
-    answer: "Private cloud",
+    answer: "(D) Private cloud",
   },
   {
     question:
       "Which of the following is a characteristic of Infrastructure as a Service (IaaS)?",
     options: [
-      "The provider manages the operating system and applications",
-      "Customers have complete control over the underlying infrastructure",
-      "The provider manages the physical hardware",
-      "Customers only pay for the services they use",
+      "(A) The provider manages the operating system and applications",
+      "(B) Customers have complete control over the underlying infrastructure",
+      "(C) The provider manages the physical hardware",
+      "(D) Customers only pay for the services they use",
     ],
     answer:
-      "Customers have complete control over the underlying infrastructure",
+      "(B) Customers have complete control over the underlying infrastructure",
   },
   {
     question:
       "Which cloud computing model provides the most flexibility to customers?",
     options: [
-      "Infrastructure as a Service (IaaS)",
-      "Platform as a Service (PaaS)",
-      "Software as a Service (SaaS)",
-      "All three models provide the same level of flexibility",
+      "(A) Infrastructure as a Service (IaaS)",
+      "(B) Platform as a Service (PaaS)",
+      "(C) Software as a Service (SaaS)",
+      "(D) All three models provide the same level of flexibility",
     ],
-    answer: "Infrastructure as a Service (IaaS)",
+    answer: "(A) Infrastructure as a Service (IaaS)",
   },
   {
     question:
       "Which cloud service model is most appropriate for an organization that wants to develop and deploy its own applications?",
     options: [
-      "Infrastructure as a Service (IaaS)",
-      "Platform as a Service (PaaS)",
-      "Software as a Service (SaaS)",
-      "Hybrid cloud",
+      "(A) Infrastructure as a Service (IaaS)",
+      "(B) Platform as a Service (PaaS)",
+      "(C) Software as a Service (SaaS)",
+      "(D) Hybrid cloud",
     ],
-    answer: "Platform as a Service (PaaS)",
+    answer: "(B) Platform as a Service (PaaS)",
   },
 ];
 
--- subjects_server/python.js
+++ subjects_server/python.js
@@ -7,190 +7,190 @@ const pythonQuestions = [
   {
     question:
       "What is the output of the following Python code?\nprint(3 < 4 and 4 > 1)",
-    options: ["True", "False", "None of the above"],
-    answer: "True",
+    options: ["(A) True", "(B) False", "(C) None of the above"],
+    answer: "(A) True",
   },
   {
     question:
       "What is the output of the following Python code?\nprint(len('hello'))",
-    options: ["5", "6", "None of the above"],
-    answer: "5",
+    options: ["(A) 5", "(B) 6", "(C) None of the above"],
+    answer: "(A) 5",
   },
   {
     question:
       "What is the output of the following Python code?\nmy_list = [1, 2, 3]\nmy_list[0] = 4\nprint(my_list)",
-    options: ["[1, 2, 3]", "[4, 2, 3]", "[4, 1, 2, 3]", "None of the above"],
-    answer: "[4, 2, 3]",
+    options: ["(A) [1, 2, 3]", "(B) [4, 2, 3]", "(C) [4, 1, 2, 3]", "(D) None of the above"],
+    answer: "(B) [4, 2, 3]",
   },
   {
     question:
       "What is the output of the following Python code?\nprint(type(3.14))",
-    options: ["int", "float", "str", "None of the above"],
-    answer: "float",
+    options: ["(A) int", "(B) float", "(C) str", "(D) None of the above"],
+    answer: "(B) float",
   },
   {
     question:
       "What is the output of the following Python code?\nprint('hello'[1:])",
-    options: ["h", "e", "llo", "ello", "None of the above"],
-    answer: "ello",
+    options: ["(A) h", "(B) e", "(C) ello", "(D) None of the above"],
+    answer: "(C) ello",
   },
   {
     question:
       "What is the output of the following Python code?\nfor i in range(3):\n    print(i)",
-    options: ["0 1 2", "1 2 3", "0 1 2 3", "None of the above"],
-    answer: "0 1 2",
+    options: ["(A) 0 1 2", "(B) 1 2 3", "(C) 0 1 2 3", "(D) None of the above"],
+    answer: "(A) 0 1 2",
   },
   {
     question: "What is the output of the following Python code?\nprint(4 % 2)",
-    options: ["1", "2", "0", "None of the above"],
+    options: ["(A) 1", "(B) 2", "(C) 0", "(D) None of the above"],
     answer: "0",
   },
   {
     question:
       "What is the output of the following Python code?\nprint(True and False)",
-    options: ["True", "False", "None of the above"],
-    answer: "False",
+    options: ["(A) True", "(B) False", "(C) None of the above"],
+    answer: "(B) False",
   },
   {
     question:
       "What is the output of the following Python code?\nmy_dict = {'a': 1, 'b': 2, 'c': 3}\nprint(my_dict.get('d'))",
-    options: ["{'a': 1, 'b': 2, 'c': 3}", "None", "0", "None of the above"],
-    answer: "None",
+    options: ["(A) {'a': 1, 'b': 2, 'c': 3}", "(B) None", "(C) 0", "(D) error"],
+    answer: "(B) None",
   },
   {
     question:
       "What is the output of the following Python code?\nprint(list(range(0, 5, 2)))",
-    options: ["[0, 1, 2, 3, 4]", "[0, 2, 4]", "[2, 4]", "None of the above"],
-    answer: "[0, 2, 4]",
+    options: ["(A) [0, 1, 2, 3, 4]", "(B) [0, 2, 4]", "(C) [2, 4]", "(D) None of the above"],
+    answer: "(B) [0, 2, 4]",
   },
   {
     question:
       "What is the output of the following Python code?\nprint(3 < 4 and 4 > 1)",
-    options: ["True", "False", "None of the above"],
-    answer: "True",
+    options: ["(A) True", "(B) False", "(C) None of the above"],
+    answer: "(A) True",
   },
   {
     question:
       "What is the output of the following Python code?\nprint(len('hello'))",
-    options: ["5", "6", "None of the above"],
-    answer: "5",
+    options: ["(A) 5", "(B) 6", "(C) None of the above"],
+    answer: "(A) 5",
   },
   {
     question:
       "What is the output of the following Python code?\nmy_list = [1, 2, 3]\nmy_list[0] = 4\nprint(my_list)",
-    options: ["[1, 2, 3]", "[4, 2, 3]", "[4, 1, 2, 3]", "None of the above"],
-    answer: "[4, 2, 3]",
+    options: ["(A) [1, 2, 3]", "(B) [4, 2, 3]", "(C) [4, 1, 2, 3]", "(D) None of the above"],
+    answer: "(B) [4, 2, 3]",
   },
   {
     question:
       "What is the output of the following Python code?\nprint(type(3.14))",
-    options: ["int", "float", "str", "None of the above"],
-    answer: "float",
+    options: ["(A) int", "(B) float", "(C) str", "(D) None of the above"],
+    answer: "(B) float",
   },
   {
     question:
       "What is the output of the following Python code?\nprint('hello'[1:])",
-    options: ["h", "e", "llo", "ello", "None of the above"],
-    answer: "ello",
+    options: ["(A) h", "(B) e", "(C) llo", "(D) ello"],
+    answer: "(D) ello",
   },
   {
     question:
       "What is the output of the following Python code?\nfor i in range(3):\n    print(i)",
-    options: ["0 1 2", "1 2 3", "0 1 2 3", "None of the above"],
-    answer: "0 1 2",
+    options: ["(A) 0 1 2", "(B) 1 2 3", "(C) 0 1 2 3", "(D) None of the above"],
+    answer: "(A) 0 1 2",
   },
   {
     question: "What is the output of the following Python code?\nprint(4 % 2)",
-    options: ["1", "2", "0", "None of the above"],
-    answer: "0",
+    options: ["(A) 1", "(B) 2", "(C) 0", "(D) None of the above"],
+    answer: "(D) 0",
   },
   {
     question:
       "What is the output of the following Python code?\nprint(True and False)",
-    options: ["True", "False", "None of the above"],
-    answer: "False",
+    options: ["(A) True", "(B) False", "(C) None of the above"],
+    answer: "(B) False",
   },
   {
     question:
       "What is the output of the following Python code?\nmy_dict = {'a': 1, 'b': 2, 'c': 3}\nprint(my_dict.get('d'))",
-    options: ["{'a': 1, 'b': 2, 'c': 3}", "None", "0", "None of the above"],
-    answer: "None",
+    options: ["(A) {' a': 1, 'b': 2, 'c': 3}", "(B) None", "(C) 0", "(D) error"],
+    answer: "(B) None",
   },
   {
     question:
       "What is the output of the following Python code?\nprint(list(range(0, 5, 2)))",
-    options: ["[0, 1, 2, 3, 4]", "[0, 2, 4]", "[2, 4]", "None of the above"],
-    answer: "[0, 2, 4]",
+    options: ["(A) [0, 1, 2, 3, 4]", "(B) [0, 2, 4]", "(C) [2, 4]", "(D) None of the above"],
+    answer: "(B) [0, 2, 4]",
   },
   {
     question:
       "What is the output of the following Python code?\nprint('Hello, world!'[7:])",
-    options: ["Hello, world!", "world!", "Hello", "None of the above"],
-    answer: "world!",
+    options: ["(A) Hello, world!", "(B) world!", "(C)  Hello", "(D) None of the above"],
+    answer: "(B) world!",
   },
   {
     question:
       "What is the output of the following Python code?\ndef my_func(x, y=3):\n return x + y\nprint(my_func(2))",
-    options: ["2", "3", "5", "None of the above"],
-    answer: "5",
+    options: ["(A) 2", "(B) 3", "(C) 5", "(D) None of the above"],
+    answer: "(C) 5",
   },
   {
     question:
       "What is the output of the following Python code?\ndef my_func(x, y):\n return x / y\nprint(my_func(10, 0))",
-    options: ["10", "0", "None", "None of the above"],
-    answer: "None",
+    options: ["(A) 10", "(B) 0", "(C) None", "(D) error"],
+    answer: "(C) None",
   },
   {
     question:
       "What is the output of the following Python code?\ndef my_func(x, y):\n return x / y\nprint(my_func(10, 2))",
-    options: ["5", "2.5", "0.5", "None of the above"],
-    answer: "5.0",
+    options: ["(A) 5", "(B) 2.5", "(C) 0.5", "(D) None of the above"],
+    answer: "(A) 5.0",
   },
   {
     question:
       "What is the output of the following Python code?\ndef my_func(x, y):\n return x * y\nprint(my_func('a', 3))",
-    options: ["'aaa'", "3a", "TypeError", "None of the above"],
-    answer: "aaa",
+    options: ["'(A) aaa'", "(B) 3a", "(C) TypeError", "(D) None of the above"],
+    answer: "(A) aaa",
   },
   {
     question:
       "What is the output of the following Python code?\nmy_list = [1, 2, 3]\nmy_list.append(4)\nprint(my_list)",
-    options: ["[1, 2, 3]", "[1, 2, 3, 4]", "4", "None of the above"],
-    answer: "[1, 2, 3, 4]",
+    options: ["(A) [1, 2, 3]", "(B) [1, 2, 3, 4]", "(C) 4", "(D) None of the above"],
+    answer: "(B) [1, 2, 3, 4]",
   },
   {
     question:
       "What is the output of the following Python code?\ndef my_func(x, y):\n return x ** y\nprint(my_func(2, 3))",
-    options: ["5", "6", "8", "None of the above"],
-    answer: "8",
+    options: ["(A) 5", "(B) 6", "(C) 8", "(D) None of the above"],
+    answer: "(C) 8",
   },
   {
     question:
       "What is the output of the following Python code?\nmy_set = {1, 2, 3, 4, 5}\nmy_set.discard(2)\nprint(my_set)",
     options: [
-      "{1, 2, 3, 4, 5}",
-      "{1, 3, 4, 5}",
-      "{2, 3, 4, 5}",
-      "None of the above",
+      "(A) {1, 2, 3, 4, 5}",
+      "(B) {1, 3, 4, 5}",
+      "(C) {2, 3, 4, 5}",
+      "(D) None of the above",
     ],
-    answer: "{1, 3, 4, 5}",
+    answer: "(B) {1, 3, 4, 5}",
   },
   {
     question:
       "What is the output of the following Python code?\ndef my_func(x, y):\n return x + y\nprint(my_func('hello', 'world'))",
-    options: ["helloworld", "hello world", "TypeError", "None of the above"],
-    answer: "helloworld",
+    options: ["(A) helloworld", "(B) hello world", "(C) TypeError", "(D) None of the above"],
+    answer: "(A) helloworld",
   },
   {
     question:
       "What is the output of the following Python code?\nmy_dict = {'a': 1, 'b': 2, 'c': 3}\nmy_dict.pop('b')\nprint(my_dict)",
     options: [
-      "{'a': 1, 'b': 2, 'c': 3}",
-      "{'a': 1, 'c': 3}",
-      "{'b': 2}",
-      "None of the above",
+      "(A) {'a': 1, 'b': 2, 'c': 3}",
+      "(B) {'a': 1, 'c': 3}",
+      "(C) {'b': 2}",
+      "(D) None of the above",
     ],
-    answer: "{'a': 1, 'c': 3}",
+    answer: "(B) {'a': 1, 'c': 3}",
   },
 ];
 
